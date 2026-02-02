aa57e773a48f4ef3f158f93aaf27903f7270c257
Steven Enamakel <enamakel@vezures.xyz>
2026-02-02 09:54:49 +0530

    chore: update package-and-publish workflow and rename installer
    - Enhanced the package-and-publish workflow to clean up the application name by removing version and tag patterns.
    - Updated the installer name from "Telegram Air" to "AlphaHuman" in the DMG creation script.

--- deploy/tauri_create_dmg.sh
+++ deploy/tauri_create_dmg.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 create-dmg \
-    --volname "Telegram Air installer" \
+    --volname "AlphaHuman installer" \
     --volicon "./tauri/icons/icon.icns" \
     --background "./tauri/images/background-dmg.tiff" \
     --window-size 540 380 \
     --icon-size 100 \
-    --icon "Telegram Air.app" 138 225 \
-    --hide-extension "Telegram Air.app" \
+    --icon "AlphaHuman.app" 138 225 \
+    --hide-extension "AlphaHuman.app" \
     --app-drop-link 402 225 \
     --no-internet-enable \
     "$1" \
